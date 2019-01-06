b3082b4e395dca8de47dc8c7145e5e3c86f79b19
ewajs <ezequiel.wajs@gmail.com>
2019-01-05 21:24:40 -0300

    Added default server ip

--- basic_request.py
+++ basic_request.py
@@ -5,7 +5,6 @@ import requests
 from datetime import datetime
 import argparse
 
-SERVER_HOST = 'localhost'
 
 def get_from_server():
     r = requests.get(SERVER_HOST)
@@ -18,20 +17,20 @@ def get_from_server():
 
 def post_to_server():
     local_time = datetime.utcnow().replace(microsecond=0).isoformat()
-    r = requests.post(f"http://{SERVER_HOST}/post_msg", 
+    r = requests.post(f"http://{SERVER_HOST}/post_msg",
                       json={"text": f"This is a test message delivered at {local_time}"})
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('server_host', metavar='S', type=str, 
-                        help='the server hos or ip')
-    parser.add_argument('method', metavar='M', type=str, 
+    parser.add_argument('--target', '-t', metavar='IP', type=str,
+                        help='the server hos or ip', default='localhost')
+    parser.add_argument('method', metavar='M', type=str,
                         help='the method for the request [POST, GET]')
 
     args = parser.parse_args()
-    
-    SERVER_HOST = args.server_host
+
+    SERVER_HOST = args.target
 
     if args.method == 'POST':
         post_to_server()
