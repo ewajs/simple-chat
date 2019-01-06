1cb30b17fa603a8a7cf4c5d64fbb9fbc3e537f31
ewajs <ezequiel.wajs@gmail.com>
2019-01-06 01:43:41 -0300

    Simple chat Workinggit status Send & Reload.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,19 +1,50 @@
 console.log("im here!!");
 
-var xhttp = new XMLHttpRequest();
-
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    document.getElementById("result").innerHTML = this.responseText;
-  }
-};
-xhttp.open("GET", "/get_history", true);
-xhttp.send();
-
+var chatbox = document.getElementById("chatbox");
+reloadMessages();
 var socket = io("http://" + document.domain + ":" + location.port);
-socket.on("connect", function() {
-  socket.emit("post_message", { data: "Sending data." }, data => {
-    console.log("Received!");
-    console.log(data);
-  });
+
+var submit = document.getElementById("submit");
+var reload = document.getElementById("reload");
+var messageInput = document.getElementById("messageInput");
+
+socket.on("server_message", data => {
+  console.log(data);
 });
+
+function appendMessage(element, text) {
+  let row = document.createElement("div");
+  row.classList = "row";
+  let col = document.createElement("div");
+  col.classList = "col-12";
+  col.innerText = text;
+  row.appendChild(col);
+  element.appendChild(row);
+}
+
+reload.addEventListener("click", reloadMessages);
+submit.addEventListener("click", sendMessage);
+
+function sendMessage() {
+  socket.emit("post_message", { text: messageInput.value }, ack => {
+    if (ack == 0) {
+      messageInput.value = "";
+      reloadMessages();
+    }
+  });
+}
+
+function reloadMessages() {
+  chatbox.innerHTML = "";
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      let response = JSON.parse(this.responseText);
+      for (let i = 0; i < response.length; i++) {
+        appendMessage(chatbox, response[i]);
+      }
+    }
+  };
+  xhttp.open("GET", "/get_history", true);
+  xhttp.send();
+}
