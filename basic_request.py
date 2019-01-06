609f6c4fbdc9975b56e7b7c4fe3833e7b563746b
m <mm@ms-MacBook-Pro.local>
2019-01-05 21:18:43 -0300

    Improve request module

--- basic_request.py
+++ basic_request.py
@@ -2,10 +2,13 @@
 
 """
 import requests
+from datetime import datetime
+import argparse
 
+SERVER_HOST = 'localhost'
 
 def get_from_server():
-    r = requests.get('http://192.168.0.14:80')
+    r = requests.get(SERVER_HOST)
     print(type(r))
     print(r.status_code)
     print(r.headers)
@@ -13,5 +16,24 @@ def get_from_server():
     print(r.text)
 
 
+def post_to_server():
+    local_time = datetime.utcnow().replace(microsecond=0).isoformat()
+    r = requests.post(f"http://{SERVER_HOST}/post_msg", 
+                      json={"text": f"This is a test message delivered at {local_time}"})
+
+
 if __name__ == "__main__":
-    get_from_server()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('server_host', metavar='S', type=str, 
+                        help='the server hos or ip')
+    parser.add_argument('method', metavar='M', type=str, 
+                        help='the method for the request [POST, GET]')
+
+    args = parser.parse_args()
+    
+    SERVER_HOST = args.server_host
+
+    if args.method == 'POST':
+        post_to_server()
+    elif args.method == 'GET':
+        get_from_server()
