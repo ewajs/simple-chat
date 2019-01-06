8e12154de4083c10ca0fc07d3b48deb248f4e267
ewajs <ezequiel.wajs@gmail.com>
2019-01-05 21:20:06 -0300

    Server recieves POST

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+import datetime
+
+from flask import Flask, request
+import sqlite3
+
 
 app = Flask(__name__)
+
+conn = sqlite3.connect('test.db')
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/post_msg', methods=['POST'])
+def post_msg():
+    print(request.json)
+    #c = conn.cursor()
+    # c.execute("INSERT INTO Message (Date, UserID, MessageText) VALUES (?, 0, ?)",
+    #         (datetime.datetime.now().isoformat(), ))
+    return "Thanks"
 
 
 if __name__ == '__main__':
